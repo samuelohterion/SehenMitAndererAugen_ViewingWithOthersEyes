704ef158654dfd1d8816306257482b80ee88fd2a
samuelohterion <samuel.ohterion@gmx.de>
2015-08-20 16:13:06 +0200

    main.cpp formatiert

--- VWOE/main.cpp
+++ VWOE/main.cpp
@@ -12,7 +12,7 @@
 using namespace std;
 
 int
-main(	int	p_argc,	char	*p_argv[] ){
+main( int p_argc, char *p_argv[ ] ) {
 
 	for( int i = 0; i < p_argc; ++i ) {
 
@@ -23,14 +23,14 @@ main(	int	p_argc,	char	*p_argv[] ){
 	sDir = "../VWOE/shaders/",
 	pDir = "../VWOE/pix/";
 
-	if(	1 < p_argc ) {
+	if( 1 < p_argc ) {
 
 		sDir = p_argv[ 1 ];
 		pDir = p_argv[ 2 ];
 	}
 
 	QApplication
-	application(	p_argc,	p_argv );
+	application( p_argc, p_argv );
 
 	GLView
 	glView( sDir, pDir );
